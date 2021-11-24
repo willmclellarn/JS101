256e510f6e6ca6109dc08cb835f1381ab2073887
willmclellarn <willmclellarn@gmail.com>
2021-11-24 13:04:17 -0800

    thxgiving 2

--- lesson_5/app.js
+++ lesson_5/app.js
@@ -1,20 +1,76 @@
-[
-  [
-    [1, 2],
-    [3, 4],
-  ],
-  [5, 6],
-].map((arr) => {
-  return arr.map((elem) => {
-    if (typeof elem === "number") {
-      // it's a number
-      return elem + 1;
-    } else {
-      // it's an array
-      return elem.map((number) => number + 1);
-    }
-  });
-});
+let arr1 = [1, [2, 3], 4];
+
+let arr2 = [{ a: 1 }, { b: 2, c: [7, 6, 5], d: 4 }, 3];
+
+let obj1 = { first: [1, 2, [3]] };
+
+let obj2 = { a: { a: ["1", "two", 3], b: 4 }, b: 5 };
+
+// let arr1 = ["a", "b", ["c", ["d", "e", "f", "g"]]];
+// console.log(arr1[2][1][3]);
+
+// let arr2 = [
+//   { first: ["a", "b", "c"], second: ["d", "e", "f"] },
+//   { third: ["g", "h", "i"] },
+// ];
+// console.log(arr2[1].third[0]);
+
+// let arr3 = [["abc"], ["def"], { third: ["ghi"] }];
+// console.log(arr3[2]["third"][0].slice(0, 1));
+
+// let obj1 = { a: ["d", "e"], b: ["f", "g"], c: ["h", "i"] };
+// console.log(obj1["b"][1]);
+
+// let obj2 = { first: { d: 3 }, second: { e: 2, f: 1 }, third: { g: 0 } };
+// console.log(Object.keys(obj2.third)[0]);
+
+// let books = [
+//   {
+//     title: "One Hundred Years of Solitude",
+//     author: "Gabriel Garcia Marquez",
+//     published: "1967",
+//   },
+//   {
+//     title: "The Great Gatsby",
+//     author: "F. Scott Fitzgerald",
+//     published: "1925",
+//   },
+//   { title: "War and Peace", author: "Leo Tolstoy", published: "1869" },
+//   { title: "Ulysses", author: "James Joyce", published: "1922" },
+//   { title: "The Book of Kells", author: "Multiple Authors", published: "800" },
+// ];
+
+// console.log(
+//   books.sort((book1, book2) => {
+//     return Number(book1.published) - Number(book2.published);
+//   })
+// );
+
+// let arr = ["10", "11", "9", "7", "8"];
+
+// console.log(
+//   arr.sort((a, b) => {
+//     return b - a;
+//   })
+// );
+
+// [
+//   [
+//     [1, 2],
+//     [3, 4],
+//   ],
+//   [5, 6],
+// ].map((arr) => {
+//   return arr.map((elem) => {
+//     if (typeof elem === "number") {
+//       // it's a number
+//       return elem + 1;
+//     } else {
+//       // it's an array
+//       return elem.map((number) => number + 1);
+//     }
+//   });
+// });
 
 // return array with 2 elements
 //
@@ -61,5 +117,3 @@
 // let sortedWords = words.sort((a, b) => {
 //   return a.length - b.length;
 // });
-
-// console.log(sortedWords);
